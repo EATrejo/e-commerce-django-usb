8faf78fb2ca0a8c5ee264a05ec47b6cbc2bf3cb0
Alonso Trejo <alonsotrejo1970@hotmail.com>
2025-02-22 13:26:50 -0600

    Subida del proyecto

---
+++ ecommerce/store/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    
+    #Store main page
+    path('', views.store, name='store'),
+
+    #Individual product
+
+    path('product/<slug:product_slug>/', views.product_info, name='product-info'),
+
+    #Indeividual category
+
+    path('search/<slug:category_slug>/', views.list_category, name='list-category'),
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
